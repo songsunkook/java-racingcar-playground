8001e9b57d486a143774155e9650adbc018f081e
songsunkook <songsunkook@gmail.com>
2023-04-07 23:43:43 +0900

    :recycle: Refactor: "Cars.Equals 오버라이딩 제거"
    테스트만을 위한 Cars.Equals 오버라이딩을 제거하고 테스트 클래스에 기존의 Cars.Equals 역할을 하는 compareCars 메서드 추가

--- src/test/java/game/RacingGameTest.java
+++ src/test/java/game/RacingGameTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -20,6 +21,7 @@ import exception.NotIntegerException;
 import exception.OverMaximumException;
 import exception.UnderMaximumException;
 
+@DisplayName("레이싱 게임에서")
 public class RacingGameTest {
     @ParameterizedTest
     @CsvSource(value = {"abc,def,ghi,jkl", "aaa,bbb,ccc,ddd", "qwe,asd,zxc,rfv"}, delimiter = ':')
@@ -47,13 +49,26 @@ public class RacingGameTest {
 
     @ParameterizedTest
     @MethodSource("carDataMethod")
+    @DisplayName("승자가 정상적으로 판단되는가")
     public void judgeFinalWinner(List<Car> carData1, List<Car> carData2) {
         Cars cars1 = new Cars();
         cars1.setCars(carData1);
         Cars cars2 = new Cars();
         cars2.setCars(carData2);
 
-        assertEquals(cars1.getFinalWinner().equals(cars2), true);
+        assertTrue(compareCars(cars1.getFinalWinner(), cars2));
+    }
+
+    private boolean compareCars(Cars cars1, Cars cars2) {
+        for (int i = 0; i < cars1.size(); i++) {
+            if (!cars1.getName(i).equals(cars2.getName(i))) {
+                return false;
+            }
+            if (cars1.getLocation(i) != cars2.getLocation(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static Stream<Arguments> carDataMethod() {
